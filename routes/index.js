96758b8dd7a2de061ef85263f59a7b2cfdd70228
Ram <nagasitaram4@gmail.com>
2021-03-30 22:34:41 +0800

    added user controller and user route

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 
 const router = express.Router();
-const homeController = require('../controllers/home_controller')
+const homeController = require('../controllers/home_controller');
+
 console.log('router loaded');
 
 router.get('/',homeController.home);
+router.use('/users',require('./users'));
 module.exports = router;
