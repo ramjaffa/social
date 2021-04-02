0b6e7b9f776876bd592221fa954ca286add0b6a8
Ram <nagasitaram4@gmail.com>
2021-04-02 14:56:27 +0800

    set up static files for layouts and partials

--- index.js
+++ index.js
@@ -2,8 +2,17 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+const expressLayouts = require('express-ejs-layouts');
+app.use(expressLayouts);//before using routes
+//extract style and scripts from sub pages into the layout
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
+
+app.use(express.static('./assets'))
 //use express router
 app.use('/',require('./routes'));
+app.set('view engine','ejs');
+app.set('views','./views');
 app.listen(port,function(err){
     if(err){
         console.log(`Error in running the server: ${err}`);
--- routes/index.js
+++ routes/index.js
@@ -7,4 +7,7 @@ console.log('router loaded');
 
 router.get('/',homeController.home);
 router.use('/users',require('./users'));
+
+// for any further routes, access from here
+//router.use('/routerName',require('./routerfile));
 module.exports = router;
